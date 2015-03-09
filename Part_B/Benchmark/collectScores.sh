5ba270e0c4ade130736b56adf8e13f207d805119
changcc5 <changcc5@cs.ucla.edu>
2015-03-09 15:55:18 -0700

    joda time cct

--- Part_B/Benchmark/collectScores.sh
+++ Part_B/Benchmark/collectScores.sh
@@ -24,3 +24,21 @@ python scoreReport.py method $reportD/passFail.csv $reportD/*cct > $reportD/meth
 
 python scoreReport.py stack $reportD/passFail.csv $reportD/*cct > $reportD/stack-scores.csv
 
+cd joda-time
+mvn clean 
+mvn compile
+mvn test
+cd ..
+reportJT=reports/joda-time
+
+./instrument-jt.sh
+
+./run-jt-tests.sh
+
+./passFailReport.sh $reportJT > $reportJT/passFail.csv
+
+python cctReport.py $reportJT/*cct > $reportJT/cctCounts.csv
+
+python scoreReport.py method $reportJT/passFail.csv $reportJT/*cct > $reportJT/method-scores.csv
+
+python scoreReport.py stack $reportJT/passFail.csv $reportJT/*cct > $reportJT/stack-scores.csv
